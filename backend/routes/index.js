f87f8b35fdaa22208c5bf44f054921df02fd3113
YohanErima <y.erima@orange.fr>
2024-04-08 07:26:47 +0200

    ajoute de prenom

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post("/", function (req, res) {
-  const { mail, age, nom, poids, taille, sexe } = req.body;
+  const { mail, age, nom, prenom, poids, taille, sexe } = req.body;
   const mb = calculateMB(poids, taille, sexe, age);
   return res.status(200).json({
     mb,
